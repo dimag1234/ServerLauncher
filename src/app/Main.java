05dc3836c9c3b606708fd217f6bd4128806359d0
Star-Barsuk <120922249+Star-Barsuk@users.noreply.github.com>
2026-03-03 02:11:36 +0700

    delete styler

--- src/app/Main.java
+++ src/app/Main.java
@@ -7,10 +7,7 @@ import logging.Loggers;
 import javax.swing.*;
 
 public class Main {
-    private static final ILogger logger = Loggers.get(Main.class);
-
     public static void main(String[] args) {
-        logger.info("123");
         SwingUtilities.invokeLater(() -> {
             JFrame frame = WindowManager.createWindow("Менеджер серверов");
         });
